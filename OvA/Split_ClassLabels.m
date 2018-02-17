7e61af48b7e9309b8567378e93c4a1daa06a9d3b
LcicC <cicco.4@live.com>
2018-02-17 11:24:36 +0100

    Del

--- OvA/Split_ClassLabels.m
+++
@@ -1,7 +0,0 @@
-function [X,Y] = Split_ClassLabels(S)
-    X = S;
-    Y = X(:,size(X,2));
-
-    %Delete labels column and normalize decimal values
-    X = X(:,1:(size(X,2))-1);
-end
--- OvA/regularizedLSTrain.m
+++
@@ -1,5 +0,0 @@
-function w = regularizedLSTrain(Xtr, Ytr, lambda)
-	n = size(Xtr,1);
-    d = size(Xtr,2);
-	w = (Xtr'*Xtr + n*lambda*eye(d))\(Xtr'*Ytr);
-end
